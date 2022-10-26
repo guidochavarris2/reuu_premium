7680f85773eacf83798b192a2dad5a92e7e420e2
Joaquin-1204 <joaquin.salas@tecsup.edu.pe>
2022-10-26 12:58:03 -0500

    funcionalidad de registro y login

---
+++ app/src/main/java/com/example/reeu_premium/URLs.java
@@ -0,0 +1,10 @@
+package com.example.reeu_premium;
+
+public class URLs {
+    private static final String ROOT_URL = "https://polar-cove-80223.herokuapp.com/registrationapi.php?apicall=";
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+    public static final String URL_LISTADO= ROOT_URL + "login_id";
+    public static final String URL_PUBLICO= ROOT_URL + "publico";
+    public static final String URL_INVITADO= ROOT_URL + "invitado";
+}
---
+++ app/src/main/java/com/example/reeu_premium/User.java
@@ -0,0 +1,64 @@
+package com.example.reeu_premium;
+
+public class User {
+    private int id;
+    private String name, apellidos, email, dni, gender;
+
+    public User(int id, String name, String apellidos, String email, String dni, String gender) {
+        this.id = id;
+        this.email = email;
+        this.dni = dni;
+        this.gender = gender;
+        this.name = name;
+        this.apellidos = apellidos;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
