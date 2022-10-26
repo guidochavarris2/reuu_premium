bbecc1d86dd47b9232a08cc8a2574d24c43c3ab4
Joaquin-1204 <joaquin.salas@tecsup.edu.pe>
2022-10-26 11:46:12 -0500

    transacción de las primeras vistas

--- app/src/main/java/com/example/reeu_premium/Login.java
+++ app/src/main/java/com/example/reeu_premium/Login.java
@@ -2,13 +2,31 @@ package com.example.reeu_premium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView tvregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tvregister=(TextView) findViewById(R.id.tvregister);
+
+        tvregister.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent nextScreen = new Intent(getApplicationContext(), Registro.class);
+                startActivity(nextScreen);
+
+            }
+        });
     }
 }
