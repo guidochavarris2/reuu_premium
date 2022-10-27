619fb1e17d61cbf443afd8689c921a1eda63a317
Aldo Pezo <aldo.pezo.m@gmail.com>
2022-10-27 18:36:17 -0500

    Commit Aldo, Lista de invitaddos

---
+++ app/src/main/java/com/example/reeu_premium/Lista_de_invitados.java
@@ -0,0 +1,22 @@
+package com.example.reeu_premium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class Lista_de_invitados extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_invitados);
+
+
+    }
+}
