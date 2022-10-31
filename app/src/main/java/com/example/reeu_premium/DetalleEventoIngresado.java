f807ed839403d0bb37b6b7e9a1c8a3c26372900c
guidochavarris2 <lucanalopezguido@gmail.com>
2022-10-30 22:58:42 -0600

    qr

--- app/src/main/java/com/example/reeu_premium/DetalleEventoIngresado.java
+++ app/src/main/java/com/example/reeu_premium/DetalleEventoIngresado.java
@@ -9,23 +9,37 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class DetalleEventoIngresado extends AppCompatActivity {
 
     Button btnQR;
+    String dato1;
+    String dato2;
+    String combo;
 
     TextView codigo;
+    private static final String AES = "AES";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_evento_ingresado);
 
+        Button ingreso = findViewById(R.id.btnIngreso);
+        TextView Codigo = findViewById(R.id.txtCodigo);
+
         recibirdetalles();
+
 
         //guido
         btnQR=(Button)findViewById(R.id.btnIngreso);
 
         codigo = (TextView) findViewById(R.id.txtCodigo);
+        /*
 
         btnQR.setOnClickListener(new View.OnClickListener() {
 
@@ -33,12 +47,61 @@ public class DetalleEventoIngresado extends AppCompatActivity {
 
                 Intent nextScreen = new Intent(getApplicationContext(), Codigo_QR_invitado.class);
                 nextScreen.putExtra("codigo", codigo.getText());
+                nextScreen.putExtra("clave", "�\u0002�\"��m��;YZeV8_");
 
                 startActivity(nextScreen);
 
 
             }
+        });*/
+
+        ingreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+                    dato2 = Codigo.getText().toString().trim();
+                    combo = dato1 + dato2;
+                    System.out.println(dato1);
+                    combo = encriptar(combo);
+
+
+                    Intent i = new Intent(DetalleEventoIngresado.this, Codigo_QR_invitado.class);
+                    i.putExtra("codigo", codigo.getText());
+                    i.putExtra("hashqr", combo);
+
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
         });
+    }
+
+    public static String encriptar(String code) throws Exception{
+        KeyGenerator KeyGenerator = javax.crypto.KeyGenerator.getInstance(AES);
+        KeyGenerator.init(128);
+        SecretKey secretKey = KeyGenerator.generateKey();
+        byte[] bytesSecretKey = secretKey.getEncoded();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(bytesSecretKey, AES);
+        Cipher cipher = Cipher.getInstance(AES);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        byte[] encriptado = cipher.doFinal(code.getBytes());
+        return new String(encriptado);
+    }
+
+    //usuario logueado DNI
+    private void buscarIDEvento() {
+
+        if(SharedPrefManager.getInstance(this).isLoggedIn()) {
+            User user = SharedPrefManager.getInstance(this).getUser();
+            dato1 = user.getDni();
+        } else {
+            Intent  intent = new Intent(DetalleEventoIngresado.this,Login.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
     public void recibirdetalles() {
         Bundle extras = getIntent().getExtras();
